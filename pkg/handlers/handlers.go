56352c39d604627d425342fd35cbaf0746600046
Mohsen Hashemi <m.hashemi.code@gmail.com>
2022-01-03 14:53:57 +0330

    send ajax request

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/hashemidesign/go-bookings-app/pkg/config"
@@ -32,7 +35,7 @@ func NewHandlers(r *Repository) {
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIp := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
-	render.RenderTemplate(w, "home.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home.page.go.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
@@ -43,27 +46,49 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIp
 
-	render.RenderTemplate(w, "about.page.go.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.page.go.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
 
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "contact.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "contact.page.go.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "make-reservation.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.go.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "generals.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "generals.page.go.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "majors.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "majors.page.go.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "search-availability.page.go.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "search-availability.page.go.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+	w.Write([]byte(fmt.Sprintf("start date is %s, and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// JsonAvailability handles request for availability and send JSON response
+func (m *Repository) JsonAvailability(w http.ResponseWriter, r *http.Request) {
+	res := jsonResponse{OK: true, Message: "Available!"}
+	out, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"html/template"
 	"log"
-	"github.com/hashemidesign/go-bookings-app/pkg/config"
-	"github.com/hashemidesign/go-bookings-app/pkg/models"
 	"net/http"
 	"path/filepath"
+
+	"github.com/hashemidesign/go-bookings-app/pkg/config"
+	"github.com/hashemidesign/go-bookings-app/pkg/models"
+	"github.com/justinas/nosurf"
 )
 
 var functions = template.FuncMap{}
@@ -19,11 +21,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 	if app.UseCache {
 		// get the template cache from app config
@@ -38,7 +41,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	}
 
 	buf := new(bytes.Buffer)
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
